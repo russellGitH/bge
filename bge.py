0c39bd52e4922408eb2d9e3e8626b6bf4a76cdcb
russellGitH <ruslansobr@gmail.com>
2025-06-29 00:32:16 +0400

    Update bge.py

--- bge.py
+++ bge.py
@@ -8,11 +8,22 @@ class sensor:
         return True
     positive = returnState
     pass
+class sensors:
+    def __getitem__(self, item):
+        return sensor()
+    pass
+
 class worldPosition:
     x=y=z=0.0
 
+class blenderObject:
+    data=None
+    def copy(self):
+        pass
+    
 class gameObject:
     name='Cube'
+    worldPosition = worldPosition()
     def __init__(self):
         Scene.objects[self.name]=self
     def __setitem__(self, key, value):
@@ -20,14 +31,23 @@ class gameObject:
 
     def __getitem__(self, key):
         return getattr(self, key)
+        
+    def get(self,value): #if own.get('start'):own['start']
+        if hasattr(self,value):return True
+        else:return False
+        
+    def replaceMesh(self, mesh_name='',visual=True,phys=True):
+        pass
 
-    worldPosition=worldPosition()
     def endObject(self):
         self=None
-
+    
+    #Text object
+    blenderObject=blenderObject()
+    
 class controller:
     owner = gameObject()
-    sensors = {'Always':sensor(),'Ray':sensor()}
+    sensors = sensors()
 
 class logic:
     @classmethod
